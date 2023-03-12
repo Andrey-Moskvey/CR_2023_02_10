638480c71e2901ad8173fcec924a88d5833baa6b
Test1Denis <ddd183a_1@mail.ru>
2023-03-12 17:39:07 +0300

    ...

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int* add(int* ar, int* n, int x) {
    int* ar1 = new int[*n + 1];
-   for (int i = 0; i < *n; i++)  ar1[i] = ar[i];
+   for (int i = 0; i < *n; i++) 
+	   ar1[i] = ar[i];
    ar1[*n] = x;
    delete [] ar;
    *n += 1;
@@ -13,17 +14,22 @@ bool isPalindrom(int num) {
 	int n = 0;
 	int* ar = new int[1];
 	while (num != 0) {
-		ar = add(ar, &n, num % 10);
+		ar = add(ar, &n, num % 10);	-- вот здесь начинается утечка памяти!!!
 		num /= 10;
 	}
 	for (int i = 0; i < n / 2; i++){
-		if (ar[i] != ar[n - i - 1]) return false;
+		if (ar[i] != ar[n - i - 1]) 
+			return false;
 	}
 	return true;
 }
 
-int main() {
+int main(int argc, char** argv) {
+	argv[1] -- название файла... задача была считывать числа из файла, и записывать результат в файл!!!
+
+
 	int n;
-	std::cin >> n;
+	std::cin >> n;	--	как вы все лихо пользуетесь std::cin!!! я передал буковки и всё хорошо отработало... и сказало что палиндром
 	std::cout << (isPalindrom(n)? "YES":"NO") << " ";
+	return 0;
 }
